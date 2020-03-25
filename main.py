fe61e9bbf80e710b7b2194dad5e44a152fb26bd6
CezaWitt <cezarywittek@gmail.com>
2020-03-25 14:08:41 +0000

    Kompassausrichtung in Winkel umrechnen

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ Punkt = 0
 while Punkt < 20:
     WertA = round(random.uniform(47.300,54.900), 3)
     WertB = round(random.uniform(6.000,15.000), 3)
-    WertC = round(random.uniform(0.000,360.000), 3)
+    WertC = 360 - round(random.uniform(0.000,360.000), 3)
     writer.writerow([WertA,WertB,WertC])
     Punkt += 1
