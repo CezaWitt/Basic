d02744d6708bf39524eb0bc646c53e466b9ef5ec
CezaWitt <cezarywittek@gmail.com>
2020-03-27 14:23:04 +0000

    Geolokalpunkte in Kompassrichtung begrenz

--- GeolocalDivision.py
+++ GeolocalDivision.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib as mlp
+import matplotlib.pyplot as plt
 deutlschland_laenge = 876
 deutschland_breite = 640
 linke_raster_grenze = 47.3
@@ -7,17 +9,34 @@ rechte_raster_grenze = 54.9
 untere_raster_grenze = 6.0
 obere_raster_grenze = 15.0
 raster_groeße = 100
+korrekturwert = 270
+spiegelung = (-1)
+winkelumrechung = np.pi / 180
+reichweite  = 2.375
 horizontale_raster_groeße = (rechte_raster_grenze - linke_raster_grenze) / raster_groeße
 vertikale_raster_groeße = (obere_raster_grenze - untere_raster_grenze) / raster_groeße
 df = pd.read_csv("Geolocal.csv", names=["Breitengrad", "Laengengrad", "Kompassausrichtung"])
-df["Breitengrad_rasterpunkt"] = df["Breitengrad"] - linke_raster_grenze
-df["Laengengrad_rasterpunkt"] = df["Laengengrad"] - untere_raster_grenze
-df["Breitenraster"] = df["Breitengrad_rasterpunkt"] / horizontale_raster_groeße
-df["Laengenraster"] = df["Laengengrad_rasterpunkt"] / vertikale_raster_groeße
+df["x_1"] = df["Breitengrad"] - linke_raster_grenze
+df["y_1"] = df["Laengengrad"] - untere_raster_grenze
+df["Breitenraster"] = df["x_1"] / horizontale_raster_groeße
+df["Laengenraster"] = df["y_1"] / horizontale_raster_groeße
 df["Breitenraster"] = df["Breitenraster"].astype(int)
 df["Laengenraster"] = df["Laengenraster"].astype(int)
-df["Raster"] = df["Laengenraster"] * 100 + df["Breitenraster"]
-df["Steigung"] = np.tan(df["Kompassausrichtung"]* np.pi / 180)
-df = df.drop(columns=["Breitengrad_rasterpunkt", "Laengengrad_rasterpunkt"])
-#df = df.drop(columns=["Breitenraster", "Laengenraster"])
+df["Start_Raster"] = df["Laengenraster"] * raster_groeße + df["Breitenraster"]
+df["Steigung"] = np.tan(((df["Kompassausrichtung"] - korrekturwert) * spiegelung) * winkelumrechung)
+df["yAchsenabschnitt"] = (df["y_1"] - (df["Steigung"] * df["x_1"]))
+df["Ankathete"] = (np.cos((df["Kompassausrichtung"] - korrekturwert) * winkelumrechung) * reichweite)
+df["x_2"] = df["x_1"] - df["Ankathete"]
+df["y_2"] = (df["Steigung"] * df["x_2"]) + df["yAchsenabschnitt"]
+#df = df.drop(columns=["x_1", "y_1"])
+df = df.drop(columns=["Breitenraster", "Laengenraster", "Steigung", "yAchsenabschnitt", "Ankathete"])
 print(df)
+plt.ylabel("Längengrad")
+plt.xlabel("Breitengrad")
+plt.figure(figsize=(6,6), dpi=80)
+plt.subplot(111)
+plt.plot([df["x_1"], df["x_2"]], [df["y_1"], df["y_2"]], color='blue', linestyle='-', marker='o')
+plt.axis([-3, 8, -2, 10])
+plt.grid(True)
+plt.show()
+plt.savefig("Koordinatensystem.png")
